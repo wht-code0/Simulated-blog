f9e101a4e7143272b99313e4b959797c95f0323c
wuhaotian <13305180235@163.com>
2019-12-14 15:48:08 +0800

    修改文章信息表单提交行为

--- public/assets/js/post-add.js
+++ public/assets/js/post-add.js
@@ -31,7 +31,7 @@ $('#feature').on('change', function () {
 // 添加文章表单提交
 $('#addForm').on('submit', function () {
     var formData = $(this).serialize();
-    
+
     $.ajax({
         type: "post",
         url: "/posts",
@@ -39,10 +39,67 @@ $('#addForm').on('submit', function () {
         success: function (response) {
             location.href = '/admin/posts.html';
             console.log(response);
-            
+
         }
     });
 
     return false;
 })
 
+// 获取浏览器传递过来的id值
+var id = getUrlParams('id');
+
+if (id != -1) {
+    $.ajax({
+        type: "get",
+        url: "/posts/" + id,
+        success: function (response) {
+            $.ajax({
+                type: "get",
+                url: "/categories",
+                success: function (categories) {
+                    response.categories = categories;
+                    // console.log(response);
+                    var html = template('modifyTpl', response);
+                    $('#parentBox').html(html);
+                }
+            });
+        }
+    });
+}
+
+// 从浏览器地址中获取参数
+function getUrlParams(name) {
+    var paramsAry = location.search.substr(1).split('&');
+    // 循环数据
+    for (var i = 0; i < paramsAry.length; i++) {
+        var tmp = paramsAry[i].split('=');
+        if (tmp[0] == name) {
+            return tmp[1];
+        }
+    }
+    return -1;
+
+}
+
+// 修改文章信息表单提交行为
+$('#parentBox').on('submit', '#modifyForm', function () {
+    var formData = $(this).serialize();
+    var id = $(this).attr('data-id');
+    console.log(formData.split('&'));
+    
+    console.log(id);
+    
+    $.ajax({
+        type: "put",
+        url: "/posts/" + id,
+        data: formData,
+        success: function (response) {
+            console.log(response);
+            
+            location.href = '/admin/posts.html'
+        }
+    });
+
+    return false;
+})
